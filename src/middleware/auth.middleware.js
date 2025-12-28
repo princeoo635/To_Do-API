333c1d6e625026089c1c98d4ac7b93ad32ae3eba
princeoo635 <princeoo635@gmail.com>
2025-12-28 22:36:51 +0530

    feat: add JWT verification middleware for secure access control

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,32 @@
+import { ApiError } from "../ApiError.js";
+import { AsyncHandler } from "../utils/AsyncHandler.js";
+import { User } from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+
+
+export const verifyJWT=AsyncHandler(
+    async (req,res,next)=>{
+        try {
+            const token=req.cookies?.accessToken || req.header("Authorization")?.
+            replace("Bearer ","")
+            
+            if(!token){
+                throw new ApiError(401,"Unauthorized Request")
+            }
+            const decodedToken=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+
+            const user=await User.findById(decodedToken._id)
+            .select("-password")
+
+            if(!user){
+                throw new ApiError(401,"Invalid access token")
+            }
+            req.user=user;
+            next();
+            
+        } catch (error) {
+            throw new ApiError(400,error?.message || "Invalid Access Token")
+        }
+    }
+)
