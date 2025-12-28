79ced660ef3db435694387510135f810e5a617e5
princeoo635 <princeoo635@gmail.com>
2025-12-28 22:36:42 +0530

    feat: add login functionality to user controller with JWT token generation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,11 @@ import { ApiError } from '../utils/ApiError.js'
 import { ApiResponse } from '../utils/ApiResponse.js'
 import { AsyncHandler } from '../utils/AsyncHandler.js'
 
+//option
+const option={
+    httpOnly: true,
+    secure: true
+}
 //Register User
 const registerUser = AsyncHandler(async (req,res) => {
     console.log(req.body);
@@ -27,7 +32,30 @@ const registerUser = AsyncHandler(async (req,res) => {
         new ApiResponse(201,createdUser,"User successfully registered.")
     )
 })
+//Login Users
+const loginUser = AsyncHandler( async (req,res) => {
+    const { email,password } = req.body
+    if(!email){
+        throw new ApiError(400,"Email is required.")
+    }
+    const user = await User.findOne({ email })
+    if( !user ){
+        throw new ApiError(404," User does not exists.")
+    }
+    const isPasswordValid = await user.isPasswordCorrect(password)
+    if( !isPasswordValid ){
+        throw new ApiError(400,"Password is invalid.")
+    }
+    const accessToken = await user.generateAccessToken()
+    const loggedInUser = await User.findById(user._id).select('-password')
+    res.status(200)
+    .cookie('accesstoken',accessToken,option)
+    .json(
+        new ApiResponse(200,loggedInUser,"user successfully logged in.")
+    )
+})
 
 export {
-    registerUser
+    registerUser,
+    loginUser
 }
