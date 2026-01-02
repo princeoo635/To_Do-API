e799eb9f490dfdfda2d0dfd3957c9e903685c851
princeoo635 <princeoo635@gmail.com>
2026-01-02 23:05:57 +0530

    feat: add task router to handle task-related API routes

--- src/app.js
+++ src/app.js
@@ -8,7 +8,9 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 import userRouter from './routes/user.route.js'
+import taskRouter from './routes/task.route.js'
 app.use("/api/v1/users",userRouter)
+app.use("/api/v1/tasks",taskRouter)
 
 
 export {app}
